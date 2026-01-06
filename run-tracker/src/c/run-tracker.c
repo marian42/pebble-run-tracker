e83c4aa87cf35eb3aa9aa76e0ce97ec60de42791
Marian Kleineberg <mail@marian42.de>
2026-01-06 22:38:21 +0000

    Implement run state and time management

--- run-tracker/src/c/run-tracker.c
+++ run-tracker/src/c/run-tracker.c
@@ -5,14 +5,44 @@ enum {
   KEY_LONGITUDE = 10001,
 };
 
+
+enum {
+  STATE_WAITING_FOR_GPS = 0,
+  STATE_READY = 1,
+  STATE_RUNNING = 2,
+  STATE_PAUSED = 3
+};
+
+static int32_t runState = STATE_WAITING_FOR_GPS;
+static uint32_t startTime;
+static uint32_t elapsedTime = 0;
+static uint32_t distanceMillimeters = 0;
+static int16_t statusUpdateTimer = 0;
+
+typedef struct {
+  float latitude;
+  float longitude;
+} Position;
+
+static Position startPosition;
+static Position lastPosition;
+
 static Window *s_window;
 static TextLayer *s_text_layer;
 static TextLayer *s_text_time;
 static TextLayer *s_text_distance;
 static TextLayer *s_text_pace;
-static TextLayer *s_text_time_unit;
+// static TextLayer *s_text_time_unit;
 static TextLayer *s_text_distance_unit;
 static TextLayer *s_text_pace_unit;
+
+static uint32_t getCurrentTime(void) {
+  time_t s;
+  uint16_t ms;
+  time_ms(&s, &ms);
+  return ((int32_t)s * 1000) + ms;
+}
+
 
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
   Tuple *latitude_kvp = dict_find(iter, KEY_LATITUDE);
@@ -35,19 +65,57 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
   int32_t intLatitude = latitude_kvp->value->int32;
   int32_t intLongitude = longitude_kvp->value->int32;
 
-  // float latitude = intLatitude * 1e-6f;
-  // float longitude = intLongitude * 1e-6f;
-  
-  /*static char formatted_position[32];
-  
-  snprintf(formatted_position, sizeof(formatted_position), "GPS: %ld.%06ld, %ld.%06ld",
-         intLatitude / 1000000, labs(intLatitude % 1000000),
-         intLongitude / 1000000, labs(intLongitude % 1000000));
-  text_layer_set_text(s_text_layer, formatted_position);*/
+  float latitude = intLatitude * 1e-6f;
+  float longitude = intLongitude * 1e-6f;
+
+  Position newPosition;
+  newPosition.latitude = latitude;
+  newPosition.longitude = longitude;
+
+  switch (runState) {
+    case STATE_WAITING_FOR_GPS:
+      runState = STATE_READY;
+      break;
+    case STATE_RUNNING:
+      distanceMillimeters += 3141;
+
+      static char formatted_distance[10];
+      
+      snprintf(formatted_distance, sizeof(formatted_distance), "%lu.%02lu", distanceMillimeters / 1000000, (distanceMillimeters / 10000) % 100);
+      text_layer_set_text(s_text_distance, formatted_distance);
+
+      break;
+  }
+
+  lastPosition = newPosition;
 }
 
 static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(s_text_layer, "Select");
+  switch (runState) {
+    case STATE_WAITING_FOR_GPS:
+      return;
+    case STATE_READY:
+      runState = STATE_RUNNING;
+      distanceMillimeters = 0;
+      elapsedTime = 0;
+      startPosition = lastPosition;
+      startTime = getCurrentTime();
+      text_layer_set_text(s_text_layer, "Run started!");
+      statusUpdateTimer = 10;
+      return;
+    case STATE_RUNNING:
+      runState = STATE_PAUSED;
+      elapsedTime += getCurrentTime() - startTime;
+      text_layer_set_text(s_text_layer, "Run paused");
+      statusUpdateTimer = 10;
+      return;
+    case STATE_PAUSED:
+      runState = STATE_RUNNING;
+      startTime = getCurrentTime();
+      text_layer_set_text(s_text_layer, "Run resumed");
+      statusUpdateTimer = 10;
+      return;
+  }
 }
 
 static void prv_up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -98,18 +166,15 @@ static void prv_window_load(Window *window) {
 
   int32_t width = 106;
   int32_t row_height = 44;
-  int32_t base_height = 18;
+  int32_t base_height = 24;
   int32_t gap = 6;
-  s_text_time = text_layer_create(GRect(0, base_height + row_height * 0, width, row_height));
+  s_text_time = text_layer_create(GRect(8, base_height + row_height * 0, bounds.size.w - 16, row_height));
   text_layer_set_text(s_text_time, "00:00");
   format_text_layer(s_text_time);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_time));
+  text_layer_set_text_alignment(s_text_time, GTextAlignmentCenter);
+  // text_layer_set_font(s_text_time, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  layer_add_child(window_layer, text_layer_get_layer(s_text_time));  
   
-  s_text_time_unit = text_layer_create(GRect(width + gap, base_height + row_height * 0 + 6, bounds.size.w - width - gap, 20));
-  text_layer_set_text(s_text_time_unit, "TIME");
-  format_text_layer_hint(s_text_time_unit);
-  layer_add_child(window_layer, text_layer_get_layer(s_text_time_unit));
-
   s_text_distance = text_layer_create(GRect(0, base_height + row_height * 1, width, row_height));
   text_layer_set_text(s_text_distance, "0.00");
   format_text_layer(s_text_distance);
@@ -122,7 +187,7 @@ static void prv_window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_text_distance_unit));
 
   s_text_pace = text_layer_create(GRect(0, base_height + row_height * 2, width, row_height));
-  text_layer_set_text(s_text_pace, "0:67");
+  text_layer_set_text(s_text_pace, "0:00");
   format_text_layer(s_text_pace);
   layer_add_child(window_layer, text_layer_get_layer(s_text_pace));
 
@@ -138,10 +203,25 @@ static void prv_window_unload(Window *window) {
 
 static void time_update_handler(struct tm *tick_time, TimeUnits units_changed)
 {
-    static char formatted_time[10];
+    if (statusUpdateTimer > 0) {
+      statusUpdateTimer -= 1;
+    } else{
+      if (runState == STATE_WAITING_FOR_GPS) {
+        text_layer_set_text(s_text_layer, "Waiting for GPS");
+      } else {
+        static char formatted_wall_time[10];
+        snprintf(formatted_wall_time, sizeof(formatted_wall_time), "%d:%02d:%02d", tick_time->tm_hour, tick_time->tm_min, tick_time->tm_sec);
+        text_layer_set_text(s_text_layer, formatted_wall_time);
+      }
+    }
     
-    snprintf(formatted_time, sizeof(formatted_time), "%d:%02d:%02d", tick_time->tm_hour, tick_time->tm_min, tick_time->tm_sec);
-    text_layer_set_text(s_text_layer, formatted_time);
+    if (runState == STATE_RUNNING) {
+      int16_t totalElapsedTime = (int16_t)((elapsedTime + (getCurrentTime() - startTime)) / 1000);
+      static char formatted_time[10];
+      
+      snprintf(formatted_time, sizeof(formatted_time), "%02d:%02d", totalElapsedTime / 60, totalElapsedTime % 60);
+      text_layer_set_text(s_text_time, formatted_time);
+    }
 }
 
 static void prv_init(void) {
